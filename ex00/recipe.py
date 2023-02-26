7d06a06f88ce7486c01934b938b183db16135f82
Two-Jay <djeeee1272@gmail.com>
2023-02-26 18:14:19 +0900

    recipe class fin

---
+++ ex00/recipe.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class RecipeType(Enum):
+    STARTER = "starter"
+    LUNCH = "lunch"
+    DESSERT = "dessert"
+
+    def __str__(self):
+        return self.value
+
+class Recipe:
+    def __init__(self, name : str, cooking_lvl : int, cooking_time : int, ingredients : list, recipe_type : str, description : str = ""):
+        self.name = name
+        self.cooking_lvl = cooking_lvl
+        self.cooking_time = cooking_time
+        self.ingredients = ingredients
+        self.recipe_type = recipe_type
+        self.description = description
+
+    def __str__(self):
+        return f"  Name: {self.name}\n\
+    - lvl {self.cooking_lvl} - for {self.recipe_type} - time : {self.cooking_time // 60:02d}h {self.cooking_time % 60:02d}m \n\
+    - Description: {self.description}\n\
+    - Ingredients: {self.ingredients}"
+
+if __name__ == "__main__":
+    recipe = Recipe("Chocolate Souffle", 4, 60, ["chocolate", "eggs", "butter"], RecipeType.DESSERT, "A delicious chocolate souffle")
+    print(recipe)
