f3e0a462d6e3ea4f46f44ac3c633272eeaa0fa30
Jeongjun Kim <jekim@c8r1s3.42seoul.kr>
2023-02-27 18:45:44 +0900

    add some testcases in test.py

--- ex02/test.py
+++ ex02/test.py
@@ -3,7 +3,29 @@ from vector import Vector
 
 class test(unittest.TestCase):
     def test(self):
-        
+        pass
+
+class test_subject_cases(unittest.TestCase):
+    def test_declare_00(self):
+        test_input = [[1.,2.,3.],]
+        vector = Vector(test_input)
+        self.assertEqual(vector.get_elem(), test_input)
+
+    def test_declare_01(self):
+        test_input = [[1.,], [2.,], [3.,]]
+        vector = Vector(test_input)
+        self.assertEqual(vector.get_elem(), test_input)
+
+    def test_declare_00(self):
+        test_input = [[1.,2.,3.],]
+        vector = Vector(test_input)
+        self.assertEqual(vector, test_input)
+
+    def test_declare_01(self):
+        test_input = [[1.,], [2.,], [3.,]]
+        vector = Vector(test_input)
+        self.assertEqual(vector, test_input)
+
 
 if __name__ == '__main__':
     unittest.main()
