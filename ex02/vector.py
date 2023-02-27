39cb54d812b0ae77ff292a9dafe06d817ff007c5
Jeongjun Kim <jekim@c8r1s3.42seoul.kr>
2023-02-27 18:46:15 +0900

    add constructor in Vector

--- ex02/vector.py
+++ ex02/vector.py
@@ -1,3 +1,21 @@
-import unittest
+from abc import ABC
 
-class test
+class Vector:
+    def __init__(self, elem : list):
+        if self.validate(list):
+            raise
+        self._elem = elem
+        self._dimention = (len(elem), len(elem[0]))
+
+    def __init__(self, vec : 'Vector'):
+        self._elem = self.get_elem()
+        self._dimention = self.get_dimention()
+    
+    def get_elem(self) -> list:
+        return self._elem
+
+    def get_dimention(self) -> tuple:
+        return self._dimention
+
+    def __repr__(self) -> list:
+        return self._elem
