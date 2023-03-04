ae86bf183e995b87de27f5f4251e91da14398724
Two-Jay <djeeee1272@gmail.com>
2023-03-04 17:32:39 +0900

    Security classes complete

---
+++ ex05/the_bank.py
@@ -0,0 +1,104 @@
+from abc import ABCMeta, abstractmethod
+
+class Account(object):
+    ID_COUNT = 1
+
+    def __init__(self, name, **kwargs):
+        self.__dict__.update(kwargs)
+        self.id = self.ID_COUNT
+        Account.ID_COUNT += 1
+        self.name = name
+    
+        if not hasattr(self, 'value'):
+            self.value = 0
+    
+        if self.value < 0:
+            raise AttributeError("Attribute value cannot be negative.")
+        if not isinstance(self.name, str):
+            raise AttributeError("Attribute name must be a str object.")
+    
+    def transfer(self, amount):
+        self.value += amount
+
+    def isEnough(self, amount):
+        return self.value >= amount
+
+class AccountCorruptionException(Exception):
+    def __init__(self):
+        pass
+
+class Security(metaclass=ABCMeta):
+    @abstractmethod
+    def inspect(self, account : Account) -> bool:
+        pass
+
+class Right_account_inspector(Security):
+    def inspect(self, account: Account) -> bool:
+        return isinstance(account, Account)
+
+class Corrupt_account_inspector(Security):
+    MANDATORY_ATTRS = ['name', 'id', 'value']
+
+    def inspect(self, account: Account) -> bool:
+        try:
+            if isEven(len(account.__dict__) % 2) == True:
+                raise AccountCorruptionException
+            if all(account.__dict__.__contains__(i) for i in self.MANDATORY_ATTRS) == False:
+                raise AccountCorruptionException
+            if type(account.__dict__['name']) != str:
+                raise AccountCorruptionException
+            if type(account.__dict__['id']) != int:
+                raise AccountCorruptionException
+            if type(account.__dict__['value']) != int and type(account.__dict__['value']) != float:
+                raise AccountCorruptionException
+            if any((key.startswith("zip") and key.startswith("addr")) == False for key in self.__dict__.keys()):
+                raise AccountCorruptionException
+            for key, value in account.__dict__.items():
+                print(f"{key} - {value}")
+                if key.startswith("b") == True:
+                    raise AccountCorruptionException
+            return True
+        except:
+            return False
+    
+class Balance_inspector(Security):
+    def inspect(self, account: Account, amount) -> bool:
+        return account.isEnough(amount)
+
+def isEven(number : int) -> bool:
+    return number % 2 == 0
+
+class Bank(object):
+    """The bank"""
+    def __init__(self):
+        self.accounts = []
+
+    def add(self, new_account):
+        """ Add new_account in the Bank
+            @new_account:  Account() new account to append
+            @return   True if success, False if an error occured
+        """
+        # test if new_account is an Account() instance and if
+        # it can be appended to the attribute accounts
+        # ... Your code  ...
+        try:
+            self.accounts.append(new_account)
+            return True
+        except:
+            return False
+
+    def transfer(self, origin, dest, amount):
+        """" Perform the fund transfer
+            @origin:  str(name) of the first account
+            @dest:    str(name) of the destination account
+            @amount:  float(amount) amount to transfer
+            @return   True if success, False if an error occured
+        """
+        # ... Your code  ...
+
+    def fix_account(self, name):
+        """ fix account associated to name if corrupted
+            @name:   str(name) of the account
+            @return  True if success, False if an error occured
+        """
+        # ... Your code  ...
