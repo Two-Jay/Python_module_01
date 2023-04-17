05beae210163797713dc82d4ca57f5aabf8adcd2
Two-Jay <djeeee1272@gmail.com>
2023-04-17 11:44:24 +0900

    update

--- ex00/recipe.py
+++ ex00/recipe.py
@@ -1,5 +1,19 @@
 from enum import Enum
 
+class Gaurd(Enum):
+    STRICT = 0
+    INHARIANT = 1
+    
+    @staticmethod
+    def validate(value, expected_type, validation_level = STRICT):
+        if validation_level == validation_level.INHARIANT:
+            if not isinstance(value, expected_type):
+                raise TypeError(f"Expected {expected_type}, got {type(value)}")
+        elif validation_level == validation_level.STRICT:
+            if type(value) != expected_type:
+                raise TypeError(f"Expected {expected_type}, got {type(value)}")
+        return value
+
 class RecipeType(Enum):
     STARTER = "starter"
     LUNCH = "lunch"
@@ -13,12 +27,12 @@ class RecipeType(Enum):
 
 class Recipe:
     def __init__(self, name : str, cooking_lvl : int, cooking_time : int, ingredients : list, recipe_type : str, description : str = ""):
-        self.name = name
-        self.cooking_lvl = cooking_lvl
-        self.cooking_time = cooking_time
-        self.ingredients = ingredients
-        self.recipe_type = recipe_type
-        self.description = description
+        self.name = Gaurd.validate(name, str)
+        self.cooking_lvl = Gaurd.validate(cooking_lvl, int)
+        self.cooking_time = Gaurd.validate(cooking_time, int)
+        self.ingredients = Gaurd.validate(ingredients, list)
+        self.recipe_type = Gaurd.validate(recipe_type, str)
+        self.description = Gaurd.validate(description, str)
 
     def __str__(self):
         return f"  Name: {self.name}\n\
